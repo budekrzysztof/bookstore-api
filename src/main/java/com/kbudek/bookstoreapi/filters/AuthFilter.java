f80ec0b3cbf3288f4dbdd5bc80b69b21971d304a
Krzysztof Budek <78693065+stiwenparker@users.noreply.github.com>
2022-06-03 18:11:18 +0200

    Add JWT authentification
    - AuthFilter.java file still contains deprecated methods that do their job but preferably will be replaced
    
    - api/categories successfuly authentificates the user with a bearer token

---
+++ src/main/java/com/kbudek/bookstoreapi/filters/AuthFilter.java
@@ -0,0 +1,54 @@
+package com.kbudek.bookstoreapi.filters;
+
+import com.kbudek.bookstoreapi.Constants;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.security.Key;
+import java.util.UUID;
+
+public class AuthFilter extends GenericFilterBean {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+
+        String authHeader = httpRequest.getHeader("Authorization");
+        if(authHeader != null) {
+            String[] authHeaderArr = authHeader.split("Bearer ");
+            if(authHeaderArr.length > 1 && authHeaderArr[1] != null) {
+                String token = authHeaderArr[1].replaceAll(" ", "").trim();
+                System.out.printf("token <%s>", token);
+                //Claims claims = Jwts.parser().setSigningKey(Constants.API_SECRET_KEY)
+                        //.parseClaimsJws(token).getBody();
+                //System.out.println(claims.getClass().getSimpleName());
+                try {
+                    Claims claims = Jwts.parser().setSigningKey(Constants.API_SECRET_KEY)
+                            .parseClaimsJws(token).getBody();
+                    //System.out.println(claims.getClass().getSimpleName());
+                    httpRequest.setAttribute("user_id", claims.get("user_id").toString().trim());  // this line to be confirmed
+                } catch (Exception e) {
+                    httpResponse.sendError(HttpStatus.FORBIDDEN.value(), "Invalid or expired token.");
+                    return;
+                }
+            } else {
+                httpResponse.sendError(HttpStatus.FORBIDDEN.value(), "Authorization token must be a Bearer [token]");
+                return;
+            }
+        } else {
+            httpResponse.sendError(HttpStatus.FORBIDDEN.value(), "Authorization token must be provided.");
+            return;
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
--- src/main/java/com/kbudek/bookstoreapi/resources/UserResource.java
+++ src/main/java/com/kbudek/bookstoreapi/resources/UserResource.java
@@ -1,7 +1,10 @@
 package com.kbudek.bookstoreapi.resources;
 
+import com.kbudek.bookstoreapi.Constants;
 import com.kbudek.bookstoreapi.domain.User;
 import com.kbudek.bookstoreapi.services.UserService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -28,7 +32,7 @@ public class UserResource {
         User user = userService.validateUser(email, password);
         Map<String, String> map = new HashMap<>();
         map.put("message", "Login successful");
-        return new ResponseEntity<>(map, HttpStatus.OK);
+        return new ResponseEntity<>(generateJWT(user), HttpStatus.OK);
     }
 
     @PostMapping("/register")
@@ -41,6 +45,20 @@ public class UserResource {
         User user = userService.registerUser(user_id, email, password);
         Map<String, String> map = new HashMap<>();
         map.put("message", "registered successfully");
-        return new ResponseEntity<>(map, HttpStatus.OK);
+        return new ResponseEntity<>(generateJWT(user), HttpStatus.OK);
+    }
+
+    private Map<String, String> generateJWT(User user) {
+        long timestamp = System.currentTimeMillis();
+        String token = Jwts.builder().signWith(SignatureAlgorithm.HS256, Constants.API_SECRET_KEY)
+                .setIssuedAt(new Date(timestamp))
+                .setExpiration(new Date(timestamp + Constants.TOKEN_VALIDITY))
+                .claim("user_id", user.getUser_id())
+                .claim("email", user.getEmail())
+                .claim("password", user.getPassword())
+                .compact();
+        Map<String, String> map = new HashMap<>();
+        map.put("token", token);
+        return map;
     }
 }
