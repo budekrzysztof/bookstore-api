28aa5866436137e8d74c99d5b844b5aa53260679
Krzysztof Budek <78693065+stiwenparker@users.noreply.github.com>
2022-06-06 22:01:33 +0200

    Delete Constants.java

--- src/main/java/com/kbudek/bookstoreapi/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.kbudek.bookstoreapi;
-
-public class Constants {
-
-    public static final String API_SECRET_KEY = "uxKLaBk84e9VRNJgljWWtF7UGJ1mi7WnD3ctWYC4Cen8xJnZn4s31I0gvGMxaviVAIIPp1Wcv3hG84uKve0fhEFt5aYJ6AWUSF0Nx5o4K8qZrJGyawzLY";
-
-    public static final long TOKEN_VALIDITY = 2 * 60 * 60 * 1000;
-}
