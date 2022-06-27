d8aa8050235c26b049de54e1ab4ad60c15a87984
Krzysztof Budek <78693065+stiwenparker@users.noreply.github.com>
2022-06-27 14:31:56 +0200

    fix incorrect mr

--- src/main/java/com/kbudek/bookstoreapi/domain/User.java
+++ src/main/java/com/kbudek/bookstoreapi/domain/User.java
@@ -1,32 +1,22 @@
 package com.kbudek.bookstoreapi.domain;
 
-import com.kbudek.bookstoreapi.services.RoleService;
-import com.kbudek.bookstoreapi.services.RoleServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.Set;
 import java.util.UUID;
 
 public class User {
     private UUID userId;
     private String email;
     private String password;
-    private Set<Role> roles;
 
     public User(UUID userId, String email, String password) {
-
-        RoleService roleService = new RoleServiceImpl();
 
         this.userId = userId;
         this.email = email;
         this.password = password;
-        this.roles = roleService.getUserRoles(userId);
 
         System.out.println("User {" +
                 "\n\tid : " + userId +
                 "\n\temail : " + email +
                 "\n\tpassword : " + password +
-                "\n\troles : " + roles.toString() +
                 "\n}");
     }
 
